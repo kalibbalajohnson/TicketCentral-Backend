9e2f4ae4acd90a8af368082c9a5fa63c8a33aee8
Johnson Kalibbala <kalijoe11@gmail.com>
2026-07-14 18:44:31 +0300

    add organiser profile

--- TicketCentral.Infrastructure/Data/AppDbContext.cs
+++ TicketCentral.Infrastructure/Data/AppDbContext.cs
@@ -7,11 +7,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
-    {
-    }
-
+    { }
     public DbSet<User> Users { get; set; }
-
     public DbSet<OrganiserProfile> OrganiserProfiles { get; set; }
     public DbSet<Event> Events { get; set; }
 
